ff9208d5cf4a7f87d577d3b93455360b90e91ca8
Esteban Andres Bustos <xXenith@protonmail.com>
2019-12-18 15:48:29 -0700

    renamed csv.py to parse_csv.py due to conflict with Python csv.py.

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def StartUp():
     PASSWORD = parser.get("bank-login", "password")
 
     chrome_options = Options()  
-    # chrome_options.add_argument("--headless")  
+    chrome_options.add_argument("--headless")  
     chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
     chrome_options.add_argument(LOG_LEVEL)
     chrome_options.binary_location = CHROME_PATH
